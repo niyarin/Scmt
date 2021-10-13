bf1882d5a3deffc3018ee3ed84d59966d8e5df6a
niyarin <inoanet@gmail.com>
2021-10-13 23:12:32 +0900

    Update bootstrap code.

--- lib/boot.scm
+++ lib/boot.scm
@@ -1,12 +1,17 @@
-(include "./opt.scm")
+(include "./scmt/opt.scm")
+(include "./scmt/command.scm")
 
 (import (scheme base)
         (scheme process-context)
         (scheme write)
-        (prefix (scmt opt-parse) scmt-optparse/))
+        (scheme load)
+        (prefix (scmt opt-parse) scmt-optparse/)
+        (prefix (scmt command) scmt-command/))
 
 (define target-scm-impl (or (get-environment-variable "SCMT_TARGET_IMPL")
                             (get-environment-variable "_SCMT_HOST_IMPL")))
+
+(define scmt-lib-dir (get-environment-variable "_SCMT_LIB_DIR"))
 
 (let ((parsed (scmt-optparse/scmt-parser (cdr (command-line)) '())))
   (case (cadr (assq 'cmd parsed))
